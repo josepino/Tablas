b860cda32a2affe282e10e7fb3bcc096535480a3
Jose Pino <joselopino@gmail.com>
2017-08-19 11:40:20 -0400

    Initial Commit

---
+++ Tablas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tablas
+//
+//  Created by Jose Pino on 19/8/17.
+//  Copyright © 2017 Jose Pino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
